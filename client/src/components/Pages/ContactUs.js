9f2302b0b5298bc063d5abb68005a7e703b840a2
bouta <boutaghousonia11@gmail.com>
2021-07-09 06:59:40 +0100

    SignInSignUp changes

---
+++ client/src/components/Pages/ContactUs.js
@@ -0,0 +1,31 @@
+import "./ContactUs.css";
+
+function ContactUs() {
+  return (
+    <div>
+      <footer>
+        <section id="ContactUs">
+          <div class="Icons">
+            <div class="FB">
+              <a href="https://www.facebook.com/sonia.boutaghou/">
+                <i class="fab fa-facebook fa-5x"></i>
+              </a>
+            </div>
+            <div class="GitHub_Icon">
+              <a href="https://github.com/Sonia-BT">
+                <i class="fab fa-github fa-5x"></i>
+              </a>
+            </div>
+            <div class="LinkedIn">
+              <a href="https://www.linkedin.com/in/sonia-boutaghou-7b79801ba/">
+                <i class="fab fa-linkedin fa-5x"></i>
+              </a>
+            </div>
+          </div>
+        </section>
+      </footer>
+    </div>
+  );
+}
+
+export default ContactUs;
