2abf868d896e9bdbc5179a5e37bfbd8f54399d6b
bouta <boutaghousonia11@gmail.com>
2021-08-06 20:36:14 +0100

    Adding images

--- server/api/controllers/event/controller.js
+++ server/api/controllers/event/controller.js
@@ -2,30 +2,41 @@ const Event = require("../../../models/event");
 const User = require("../../../models/user");
 
 const addEvent = async (req, res) => {
-  const {
-    eventName,
-    eventDescription,
-    eventImage,
-    category,
-    format,
-    eventDate,
-    Online,
-  } = req.body;
+  try {
+    const {
+      eventName,
+      eventDescription,
+      eventImage,
+      category,
+      format,
+      eventDate,
+      Online,
+    } = req.body;
 
-  const event = new Event({
-    eventName,
-    eventDescription,
-    eventImage,
-    category,
-    format,
-    eventDate,
-    Online,
-  });
-  await event.save();
-  res.status(201).json({
-    message: "Successfully added",
-    data: event,
-  });
+    //check if there is a event with the same eventName
+    const foundEventName = await Event.findOne({ eventName });
+    if (foundEventName) {
+      return res.status(403).json({ error: "eventName is already used" });
+    }
+
+    const event = new Event({
+      eventName,
+      eventDescription,
+      eventImage,
+      category,
+      format,
+      eventDate,
+      Online,
+    });
+    await event.save();
+    res.status(201).json({
+      message: "New event Successfully added",
+      data: event,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(errors);
+  }
 };
 
 module.exports = {
