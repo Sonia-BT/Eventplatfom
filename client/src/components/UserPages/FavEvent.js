ffbe49e16afe9841802f732375f8b850ec6f4cd4
bouta <boutaghousonia11@gmail.com>
2021-10-15 06:20:20 +0100

    no change

---
+++ client/src/components/UserPages/FavEvent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./FavEvent.css";
+const FavEvent = () => {
+  return (
+    <div>
+      <h1>Hello Fav World !!</h1>
+    </div>
+  );
+};
+
+export default FavEvent;
